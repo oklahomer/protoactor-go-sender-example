6202960b2e65628096c607ff6abefe24525f2ea5
Oklahomer <hagiwara.go@gmail.com>
2019-08-17 14:45:11 +0900

    Update gRPC based cluster sample

--- cluster/cluster-ping-grpc/main.go
+++ cluster/cluster-ping-grpc/main.go
@@ -32,7 +32,7 @@ func (p *pingActor) Receive(ctx actor.Context) {
 			log.Print(err.Error())
 			return
 		}
-		log.Printf("Received %#v", pong)
+		log.Printf("Received %v", pong)
 
 	case *messages.Pong:
 		// Never comes here.
@@ -50,10 +50,12 @@ func main() {
 	}
 	cluster.Start("cluster-grpc-example", "127.0.0.1:8081", cp)
 
-	pingProps := actor.FromProducer(func() actor.Actor {
+	rootContext := actor.EmptyRootContext
+
+	pingProps := actor.PropsFromProducer(func() actor.Actor {
 		return &pingActor{}
 	})
-	pingPid := actor.Spawn(pingProps)
+	pingPid := rootContext.Spawn(pingProps)
 
 	finish := make(chan os.Signal, 1)
 	signal.Notify(finish, os.Interrupt)
@@ -65,11 +67,11 @@ func main() {
 	for {
 		select {
 		case <-ticker.C:
-			pingPid.Tell(struct{}{})
+			rootContext.Send(pingPid, struct{}{})
 
 		case <-finish:
-			return
 			log.Print("Finish")
+			return
 
 		}
 	}
--- cluster/cluster-pong-grpc/main.go
+++ cluster/cluster-pong-grpc/main.go
@@ -16,6 +16,11 @@ type ponger struct {
 	cluster.Grain
 }
 
+var _ messages.Ponger = (*ponger)(nil)
+
+func (*ponger) Terminate() {
+}
+
 func (*ponger) SendPing(ping *messages.Ping, ctx cluster.GrainContext) (*messages.Pong, error) {
 	// The sender process is not a sending actor, but a future process
 	log.Printf("Sender: %+v", ctx.Sender())
@@ -33,7 +38,7 @@ func main() {
 
 	// kind name must be the same one as protos.proto's service name
 	// This name is set in PongerGrain.SendPingWithOpts
-	remote.Register("Ponger", actor.FromProducer(func() actor.Actor {
+	remote.Register("Ponger", actor.PropsFromProducer(func() actor.Actor {
 		return &messages.PongerActor{}
 	}))
 
@@ -44,7 +49,7 @@ func main() {
 	cluster.Start("cluster-grpc-example", "127.0.0.1:8080", cp)
 
 	finish := make(chan os.Signal, 1)
-	signal.Notify(finish, os.Interrupt)
+	signal.Notify(finish, syscall.SIGINT)
 	signal.Notify(finish, syscall.SIGTERM)
 	<-finish
 	log.Print("Finish")
